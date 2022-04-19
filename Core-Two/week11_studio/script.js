b903237526da7c17bbf1d728380902288f510ed1
chrissywang1104 <70614568+chrissywang1104@users.noreply.github.com>
2022-04-19 18:34:34 -0400

    studio

--- Core-Two/week11_studio/script.js
+++ Core-Two/week11_studio/script.js
@@ -1,31 +1,101 @@
-// fetch data from airtable
-fetch('https://api.airtable.com/v0/app3Okb7D9jJAXdkz/Christina',{
-    headers: {
-        Authorization: 'Bearer keym515eYuin1dnCw', // API key (from Airtable account)
+function fetchData(emotion) {
+    fetch('https://api.airtable.com/v0/app3Okb7D9jJAXdkz/Christina',{
+        headers: {
+            Authorization: 'Bearer keym515eYuin1dnCw', // API key (from Airtable account)
+        }
+    })
+    .then(response => response.json())
+    .then(data => { 
+        showData(data, emotion);
+    });
+}
+
+ 
+
+function showData(data, emotion) {
+    const albumsContainer = document.querySelector('.christina-container'); 
+
+    var html = `<div class="row">`;
+    var index = 0;
+    var i;
+    for (i = 0; i < data.records.length; i++) {
+        if (data.records[i].fields.emotion == emotion) {
+            html += `
+            <div class="col">
+                </br>
+                <h5 style="color:white; font-family: 'Roboto Mono'; font-size: 2.1em;">${data.records[i].fields.year}${data.records[i].fields.date}</h5>
+                <img src="${data.records[i].fields.pictures[0].thumbnails.large.url}" class="rounded-circle" width='200'/>
+            </div>
+            `;      
+            if (index % 3 == 2) {
+                html += `</div><div class="row">`;
+            }
+            index++;
+        }
     }
-})
-    .then(response => response.json()) // tells JS to expect data in json format
-    .then(data => {
-        // all codes inside here
 
-        console.log(data); // **log out of my data, explore in the browser console
+    if (index % 3 == 1) {
+        html += `<div class="col"></div><div class="col"></div>`;
+    } else if (index % 3 == 2) {
+        html += `<div class="col"></div>`;
+    }
 
-        const albumsContainer = document.querySelector('.christina-container'); // tell JS about the div added to the HTML file - to put content inside
+    html += `</div>`;
+    albumsContainer.innerHTML = html;
+}
 
-        // loop over each record (row) of the Airtable data
-        
-        albumsContainer.innerHTML += 
-        
-        albumsContainer.innerHTML += `
-          <div class="album">
-            <div class="top-band" style="width: ${(album.fields.my_rating / 5) * 100}%"></div>
-            <h5>${christina.fields.release_year}</h5>  
-            <h3>${christina.fields.title}</h3>
-            <h4>${christina.fields.artist}</h4>
-            <img src="${christina.fields.pictures[0].thumbnails.large.url}" width'200' />
-          </div>
-      `;      
-        
-    )});
 
-  
+// function showAll() {
+//     fetchData(data.records[i].fields.emotion);
+// }
+function showPicturesForDrinking() {
+    fetchData("drinking");
+}
+
+function showPicturesForEating() {
+    fetchData("eating");
+}
+function showPicturesForExcited() {
+    fetchData("excited");
+}
+function showPicturesForFrustrated() {
+    fetchData("frustrated");
+}
+function showPicturesForFunny() {
+    fetchData("funny");
+}
+function showPicturesForHappy() {
+    fetchData("happy");
+}
+function showPicturesForStare() {
+    fetchData("stare");
+}
+function showPicturesForLaugh() {
+    fetchData("laugh");
+}
+function showPicturesForSad() {
+    fetchData("sad");
+}
+function showPicturesForSmile() {
+    fetchData("smile");
+}
+function showPicturesForWorried() {
+    fetchData("worried");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
